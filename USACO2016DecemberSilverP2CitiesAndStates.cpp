21e23b4bbb5418783ffdb1358967676f70eb1844
AidenWang0511 <70038087+AidenWang0511@users.noreply.github.com>
2021-10-29 21:50:42 -0400

    Update USACO2016DecemberSilverP2CitiesAndStates.cpp

--- USACO2016DecemberSilverP2CitiesAndStates.cpp
+++ USACO2016DecemberSilverP2CitiesAndStates.cpp
@@ -8,8 +8,8 @@ int main(){
     cin.sync_with_stdio(0);
     cin.tie(0);
 
-    // freopen("citystate.in","r",stdin);
-	// freopen("citystate.out","w",stdout);
+    freopen("citystate.in","r",stdin);
+	freopen("citystate.out","w",stdout);
 
     cin>>N; 
     for(int i=0; i<N; i++){
@@ -19,15 +19,17 @@ int main(){
         s = a+b;
         m[s]++;
     }
-    int ans = 0;
+    long long ans = 0;
     
     map<string, int>::iterator it = m.begin();
     for(it = m.begin(); it != m.end(); it++){
-        cout<<it->first<<" "<<it->second<<endl;
         string s = it->first;
         string a = s.substr(0,2);
         string b = s.substr(2,2);
         string s2 = b+a;
+        if(s2 == s){
+            continue;
+        }
         if(m.count(s2) > 0){
             ans += m[s] * m[s2];
             m[s] = 0;
