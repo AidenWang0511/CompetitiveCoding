ceab549fdeb9724b2119345afa7a8c9800ba9859
AidenWang0511 <70038087+AidenWang0511@users.noreply.github.com>
2021-11-12 20:43:48 -0500

    Update USACO2020DecemberSilverP1Cowntagion.cpp

--- USACO2020DecemberSilverP1Cowntagion.cpp
+++ USACO2020DecemberSilverP1Cowntagion.cpp
@@ -1,6 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//BFS function
+void bfs(vector<vector<int>>&adj,vector<int>&visited,int s){
+    queue<int>q;
+    q.push(s);
+    visited[s]=1;
+    while(!q.empty()){
+        int u=q.front();
+        q.pop();
+        for(int i=0;i<adj[u].size();i++){
+            if(visited[adj[u][i]]==0){
+                visited[adj[u][i]]=1;
+                q.push(adj[u][i]);
+            }
+        }
+    }
+}
+
+
 int main(){
     cin.sync_with_stdio(0);
     cin.tie(0);
